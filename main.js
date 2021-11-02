31bbd851ced1f2dcc01847b6e9741f32d84825dc
bradley <bradleybkempton@gmail.com>
2021-11-01 17:47:50 -0700

    Added eventListener for hover class and reset. Edited html with headers from DOM.

--- main.js
+++ main.js
@@ -1,17 +1,49 @@
-//container
+//Header
+const head = document.createElement('h1');
+head.textContent = "Etch-a-Sketch";
+document.body.appendChild(head);
+
+//Container
 const container = document.createElement('div');
 container.classList.add('container');
 document.body.appendChild(container);
 
-//row
-//const container = document.createElement('div');
-//container.classList.add('container');
-//document.body.appendChild(container);
-
-
-//let e = document.body;
+//Create Div
 for (let i = 0; i < 256; i++) {
     const row = document.createElement('div');
-    row.className = "row-div";
-    container.appendChild(row);
+    row.classList.add("row-div");
+    row.classList.add("write");
+    container.appendChild(row);  
 }
+
+//Mark grid black on hover
+document.querySelectorAll('.write').forEach(item => {
+    item.addEventListener('mouseover', event => {
+        item.style.cssText ="background-color: black;"
+    })
+  });
+
+
+//Reset background color to white
+ //const reset = document.querySelector(".container");
+ const reset = document;
+
+ reset.addEventListener("click", () => {
+    document.querySelectorAll('.write').forEach(item => {
+     item.style.cssText ="background-color: white;"
+    })
+ });
+
+//Instructions
+const inst = document.createElement('h3');
+inst.textContent = "CLICK ANYWHERE TO RESET";
+document.body.appendChild(inst);
+
+
+
+
+
+
+
+
+
