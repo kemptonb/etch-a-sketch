3fb26d99a091c97c39e6d69ada815c47452039d0
bradley <bradleybkempton@gmail.com>
2021-11-02 11:05:25 -0700

    Edited prompt input conditionals to take out null, string, nonconforming numbers. Edited read me. Cleaned code.

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # etch-a-sketch
 Javascript Etch-a-Sketch 
 
-DOM manipulation to create Etch-a-Sketch game.
+Skills demonstrated:
+
+--DOM manipulation
+--Loops, conditionals
+--Flexbox UI
+--Interpolation of prompt input var assigned to DOM style alteration
+
+All content is rendered using JavaScript DOM manipulation. A loop renders a 16 x 16 square grid ordered with flexbox. Each div in the grid has a mouse-over event listener that marking black any square the mouse hovers over. 
+
+If the user clicks once, the div squares receive a background of white, returning the grid to its default, unmarked state.  
+
+Upon a double-click, the DOM rendering of the 16 x 16 grid is erased. A prompt appears asking for the new grid width, only accepting numbers greater than 0. If greater than 100, 100 is set as the default width.
+
+Once the grid width is selected, the new grid is rendered conforming to the prompt’s input value. A new mouse-over event listener is placed upon the grid, allowing it be marked, drawn upon. 
--- main.js
+++ main.js
@@ -1,31 +1,30 @@
 //Header
-const head = document.createElement('h1');
-head.textContent = "Etch-a-Sketch";
+const head = document.createElement("h1");
+head.textContent = "ETCH-A-SKETCH";
 document.body.appendChild(head);
 
 //Container
-const container = document.createElement('div');
-container.classList.add('container');
+const container = document.createElement("div");
+container.classList.add("container");
 document.body.appendChild(container);
 
-//Create Div
+//Create div
 createGrid();
 
-//Reset background text
-const inst = document.createElement('h3');
+//Reset background header text to black slate
+const inst = document.createElement("h3");
 inst.textContent = "CLICK ANYWHERE TO RESET";
 document.body.appendChild(inst);
 
-//Change grid instructions
-const inst2 = document.createElement('h3');
+//Change grid header text
+const inst2 = document.createElement("h3");
 inst2.textContent = "DOUBLE-CLICK TO CHANGE GRID SIZE";
 document.body.appendChild(inst2);
-
 
 const reset = document;
 
 reset.addEventListener("click", () => {
-    document.querySelectorAll('.write').forEach(item => {
+    document.querySelectorAll(".write").forEach(item => {
         item.style.cssText = "background-color: white;"
     })
 });
@@ -45,7 +44,7 @@ reset.addEventListener("dblclick", () => {
 //Create grid function
 function createGrid() {
     for (let i = 0; i < 256; i++) {
-        const row = document.createElement('div');
+        const row = document.createElement("div");
         row.classList.add("row-div");
         row.classList.add("write");
         container.appendChild(row);
@@ -53,20 +52,25 @@ function createGrid() {
     markGrid();
 }
 
-//Change grid function
+//Change grid size function
 function calculateGrid() {
     let gridWidth = prompt("Enter grid width:");
-    if (gridWidth > 100){
+    if (gridWidth > 100) {
+        console.log("Over 100");
         gridWidth = 100;
-    } else if (gridWidth < 0) {
-        gridWidth = prompt("Invalid Answer. Enter grid width:");
+    } else if (gridWidth > 0 && gridWidth <= 100) {
+        gridWidth;
+        console.log("Just right" + gridWidth);
+    } else {
+        alert("Invalid answer");
+        calculateGrid();
     }
 
     console.log(gridWidth);
     let gridWidth2 = gridWidth * gridWidth;
     for (let i = 0; i < gridWidth2; i++) {
         let toCss = `grid-template-columns: repeat(${gridWidth}, 1fr);`
-        const row = document.createElement('div');
+        const row = document.createElement("div");
         row.classList.add("row-div");
         row.classList.add("write");
         container.style.cssText = toCss;
@@ -74,16 +78,16 @@ function calculateGrid() {
     }
     markGrid();
 
-    function gridWidthMax(){
-        
+    function gridWidthMax() {
+
     }
 }
 
 //Hover to mark grid function
-function markGrid(){
-document.querySelectorAll('.write').forEach(item => {
-    item.addEventListener('mouseover', event => {
-        item.style.cssText = "background-color: black;"
-    })
-});
+function markGrid() {
+    document.querySelectorAll(".write").forEach(item => {
+        item.addEventListener("mouseover", event => {
+            item.style.cssText = "background-color: black;"
+        })
+    });
 }
